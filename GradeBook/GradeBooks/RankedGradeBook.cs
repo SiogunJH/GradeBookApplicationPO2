29a9d61be64173d39fa3715b7fab92d916d3c09b
Jakub <homarofficial@gmail.com>
2023-03-07 12:58:11 +0100

    Ranked CalcStat

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,6 +10,14 @@ namespace GradeBook.GradeBooks
             this.Type = GradeBookType.Ranked;
         }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            else
+                base.CalculateStatistics();
+        }
+
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5) throw new InvalidOperationException();
