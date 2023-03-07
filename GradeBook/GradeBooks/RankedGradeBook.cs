e89b65ad6ad18beebe2b1ed5a4a332f30216b18b
Jakub <homarofficial@gmail.com>
2023-03-07 12:51:38 +0100

    Ranked Grading

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,3 +1,4 @@
+using System;
 using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
@@ -7,6 +8,23 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             this.Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5) throw new InvalidOperationException();
+
+            int howManyBetter = 0;
+            foreach (var Student in Students)
+                if (averageGrade < Student.AverageGrade) howManyBetter++;
+
+            int percent = (100 * howManyBetter) / Students.Count;
+
+            if (percent < 20) return 'A';
+            if (percent < 40) return 'B';
+            if (percent < 60) return 'C';
+            if (percent < 80) return 'D';
+            return 'F';
         }
     }
 }
